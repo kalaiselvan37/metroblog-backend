70d1beb3ff5846fe902979f35bc4f11e3f3f792e
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-07-08 21:00:01 +0530

    bbb

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'))
 app.use(compression())
 app.use('/uploads', express.static('uploads'))
 app.use(cors({
-  origin: true,
+  origin: 'https://metroblog-frontend.vercel.app',
   credentials: true
 }))
 
