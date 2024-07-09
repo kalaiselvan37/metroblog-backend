99b8375bbb6ffbf9f4fe649b10da4166c30c9148
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-07-09 17:40:25 +0530

    newchangs4

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { SaveUser } from '../controllers/User/UserController.js';
+import { SaveUser } from '../controllers/User/userController.js';
 
 const UserRouter = express.Router()
    UserRouter.post('/saveuser' , SaveUser)
