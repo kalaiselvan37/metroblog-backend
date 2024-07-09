603df2a8b65f0e9dd1cd74316194bceedaca69ee
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-07-09 12:35:54 +0530

    Upd

--- src/app.js
+++ src/app.js
@@ -1,30 +1,19 @@
-import express from 'express';
-import compression from 'compression';
-import morgan from 'morgan';
-import cors from 'cors';
-import router from './routes/routes.js';
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
-app.use(compression());
-app.use('/uploads', express.static('uploads'));
-
-const corsOptions = {
-  origin: 'https://metroblog-frontend.vercel.app',
+import express from 'express'
+import compression from 'compression'
+import morgan from 'morgan'
+import cors from 'cors'
+import router from './routes/routes.js'
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+app.use(compression())
+app.use('/uploads', express.static('uploads'))
+app.use(cors({
+  origin: true,
   credentials: true
-};
+}))
 
-app.use(cors(corsOptions));
-
-// Handle preflight requests
-app.options('*', cors(corsOptions), (req, res) => {
-  res.sendStatus(200);
-});
-
-app.use('/', router);
-app.get('/', (req, res) => res.send('Welcome to Metro Blogs'));
-
-export default app;
+app.use('/', router)
+app.get('/', (req, res) => res.send('Welcome to Metro Blogs'))
+export default app
