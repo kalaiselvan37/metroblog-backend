f06f68cae685f3184bee85cfab3a36b3f1197c8c
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-07-09 11:51:11 +0530

    new3

--- src/app.js
+++ src/app.js
@@ -6,12 +6,6 @@ import router from './routes/routes.js';
 
 const app = express();
 
-// Log every request
-app.use((req, res, next) => {
-  console.log(`Request received: ${req.method} ${req.path}`);
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
@@ -25,17 +19,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Handle preflight requests
 app.options('*', cors(corsOptions), (req, res) => {
   res.sendStatus(200);
-});
-
-// Log CORS headers for each response
-app.use((req, res, next) => {
-  res.on('finish', () => {
-    console.log('CORS headers:', res.getHeaders()['access-control-allow-origin']);
-  });
-  next();
 });
 
 app.use('/', router);
