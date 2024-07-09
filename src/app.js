7ae385f93c81b282404b068128affd7397e72fa1
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-07-09 11:39:20 +0530

    new3

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ import router from './routes/routes.js';
 
 const app = express();
 
+// Log every request
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.path}`);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
@@ -20,7 +26,17 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight requests
-app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions), (req, res) => {
+  res.sendStatus(200);
+});
+
+// Log CORS headers for each response
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    console.log('CORS headers:', res.getHeaders()['access-control-allow-origin']);
+  });
+  next();
+});
 
 app.use('/', router);
 app.get('/', (req, res) => res.send('Welcome to Metro Blogs'));
